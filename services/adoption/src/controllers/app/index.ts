111c32d2958d1bde106cd3fe1af7c3a20448c501
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-03-30 23:59:47 +0600

    zk: add controllers

--- services/adoption/src/controllers/app/index.ts
+++ services/adoption/src/controllers/app/index.ts
@@ -15,3 +15,170 @@ routes.delete("/applyPet/:id", applyPet);
 const applyPet = require("./applyPet");
 
 module.exports = routes;
+
+// Import required modules
+const express = require("express");
+
+// Create an instance of Express
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample data for users
+let users = [];
+
+// User controllers
+const createUser = (req, res) => {
+  const { username, email } = req.body;
+  const newUser = { id: users.length + 1, username, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
+
+const getUsers = (req, res) => {
+  res.json(users);
+};
+
+// Routes
+app.post("/users", createUser);
+app.get("/users", getUsers);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+// Import required modules
+const express = require("express");
+
+// Create an instance of Express
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample data for users
+let users = [];
+
+// User controllers
+const createUser = (req, res) => {
+  const { username, email } = req.body;
+  const newUser = { id: users.length + 1, username, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
+
+const getUsers = (req, res) => {
+  res.json(users);
+};
+
+// Routes
+app.post("/users", createUser);
+app.get("/users", getUsers);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Import required modules
+const express = require("express");
+
+// Create an instance of Express
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample data for users
+let users = [];
+
+// User controllers
+const createUser = (req, res) => {
+  const { username, email } = req.body;
+  const newUser = { id: users.length + 1, username, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
+
+const getUsers = (req, res) => {
+  res.json(users);
+};
+
+// Routes
+app.post("/users", createUser);
+app.get("/users", getUsers);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+// Import required modules
+const express = require("express");
+
+// Create an instance of Express
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample data for users
+let users = [];
+
+// User controllers
+const createUser = (req, res) => {
+  const { username, email } = req.body;
+  const newUser = { id: users.length + 1, username, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
+
+const getUsers = (req, res) => {
+  res.json(users);
+};
+
+// Routes
+app.post("/users", createUser);
+app.get("/users", getUsers);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+// Import required modules
+const express = require("express");
+
+// Create an instance of Express
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample data for users
+let users = [];
+
+// User controllers
+const createUser = (req, res) => {
+  const { username, email } = req.body;
+  const newUser = { id: users.length + 1, username, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
+
+const getUsers = (req, res) => {
+  res.json(users);
+};
+
+// Routes
+app.post("/users", createUser);
+app.get("/users", getUsers);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
