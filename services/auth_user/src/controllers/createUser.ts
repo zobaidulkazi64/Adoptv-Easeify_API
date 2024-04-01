9fefc1a5e52ee82fb9eb2d44482a07f604406962
Zobaidul Kazi <zobaidul@kazi.zobaidul>
2024-04-01 21:30:56 +0600

    add user schema

--- services/auth_user/src/controllers/createUser.ts
+++ services/auth_user/src/controllers/createUser.ts
@@ -1,14 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import z from "zod";
 
+const createUserSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
 export async function createUser(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const createUserSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
+  try {
+    const paresBody = createUserSchema.parseAsync(req.body);
+
+    console.log(paresBody);
+  } catch (error) {
+    next(error);
+    console.log(error);
+  }
 }
