9b26128d48e3eccf532172b3f7be776d9eae3bca
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-02 23:26:36 +0600

    as

--- services/adoption/src/controllers/allps.mts
+++ services/adoption/src/controllers/allps.mts
@@ -3,3 +3,16 @@ import { Request, Response, NextFunction } from "express";
 export const ab = (req: Request, res: Response, next: NextFunction) => {
   res.status(403).json({ message: "Forbidden" });
 };
+
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Not Found" });
+};
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(500).json({ message: "Internal Server Error" });
+};
--- services/adoption/src/controllers/applyPet.ts
+++ services/adoption/src/controllers/applyPet.ts
@@ -1,7 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 
-const applyPet = (req: Request, res: Response, next: NextFunction) => {
- 
-};
+const applyPet = (req: Request, res: Response, next: NextFunction) => {};
 
 export default applyPet;
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Not Found" });
+};
+
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(500).json({ message: "Internal Server Error" });
+};
