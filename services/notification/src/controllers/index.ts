7be181db90c2440eae276f5dfc03b859623203ae
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-14 06:00:37 +0600

    pet: update noti:controller

--- services/notification/src/controllers/index.ts
+++ services/notification/src/controllers/index.ts
@@ -3,3 +3,7 @@ import { Request, Response, NextFunction } from "express";
 const controller = (req: Request, res: Response, next: NextFunction) => {
   res.send("Notification service is up and running!");
 };
+
+const controller2 = (req: Request, res: Response, next: NextFunction) => {
+  res.send("Notification service is up and running!");
+};
