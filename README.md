796106d7c311c2c3b520f5ee9b4f3cb196e0a243
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-17 23:22:51 +0600

    work-api

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Pet Adoption and Rescue Platform
 
 ## Overview
+
 [Overview](./doc.md)
 
-
 ## ER Diagram
-[ER Diagram](https://drive.google.com/file/d/1aD4H5zom47H0mSEFSMPGYwtpM-pK-3GD/view?usp=sharing)
+
+[ER Diagram](https://viewer.diagrams.net/?tags=%7B%7D&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Adoptv-Easeify_API%20ER.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1aD4H5zom47H0mSEFSMPGYwtpM-pK-3GD%26export%3Ddownload)
 
 ## Contributors
 
--- services/auth_user/src/schemas.ts
+++ services/auth_user/src/schemas.ts
@@ -4,10 +4,27 @@ export const UserCreateSchema = z.object({
   authUserId: z.string(),
   name: z.string(),
   email: z.string().email(),
-  address: z.string().optional(),
-  phone: z.string().optional(),
+  password: z.string().min(6).max(200),
 });
 
-export const UserUpdateSchema = UserCreateSchema.omit({
-  authUserId: true,
-}).partial();
+export const UserLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(200),
+});
+
+export const UserUpdateSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6).max(200),
+});
+
+export const UserGetSchema = z.object({
+  id: z.string().cuid(),
+  email: z.string().email(),
+  name: z.string(),
+  password: z.string().min(6).max(200),
+});
+
+export const UserDeleteSchema = z.object({
+  id: z.string().cuid(),
+});
